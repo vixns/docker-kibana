7a3631fedb1205d65243219ed891672e4586350a
Stéphane Cottin <stephane.cottin@vixns.com>
2015-03-01 15:49:49 +0100

    first commit

---
+++ kibana.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -z "$ELASTICSEARCH_URI" ]
+then
+  # ask consul
+  export ELASTICSEARCH_URI="http://$(dig-srv ${ELASTICSEARCH_FQDN-elasticsearch.service.consul} | awk -F',' '{print $1}')"
+fi
+
+sed -i -e "s#^elasticsearch_url: .*\$#elasticsearch_url: \"${ELASTICSEARCH_URI}\"#g" /opt/kibana/config/kibana.yml
+
+exec /opt/kibana/bin/kibana
