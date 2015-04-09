6324c933d2512a49c144e9db4eca3a136da5ef37
Stéphane Cottin <stephane.cottin@vixns.com>
2015-04-09 13:03:26 +0200

    remove consul dns support, please use consul-template

--- kibana.sh
+++ kibana.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-if [ -z "$ELASTICSEARCH_URI" ]
+if [ -n "$ELASTICSEARCH_URI" ]
 then
-  # ask consul
-  export ELASTICSEARCH_URI="http://$(dig-srv ${ELASTICSEARCH_FQDN-elasticsearch.service.consul} | awk -F',' '{print $1}')"
+  sed -i -e "s#^elasticsearch_url: .*\$#elasticsearch_url: \"${ELASTICSEARCH_URI}\"#g" /opt/kibana/config/kibana.yml	
 fi
-
-sed -i -e "s#^elasticsearch_url: .*\$#elasticsearch_url: \"${ELASTICSEARCH_URI}\"#g" /opt/kibana/config/kibana.yml
 
 unset HOST
 unset PORT
